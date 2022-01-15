74c6d1dbd31c8af75fdbc0382db3809543482b7b
pilotvollmar <mrmichaelkendall@gmail.com>
2022-01-15 00:23:18 +0000

    Configuring Mongoose

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	env: {
+		MONGO_URI:
+			'mongodb+srv://heroku:vxPhya1jICeFlYco@mglk.hhtac.mongodb.net/mglktech-next?retryWrites=true&w=majority',
+	},
+};
