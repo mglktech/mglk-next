291eac59082cf4f632136814eb13b00ca79f209f
pilotvollmar <mrmichaelkendall@gmail.com>
2022-06-12 12:28:21 +0100

    implementing spotify widget

---
+++ components/Navigation/SpotifyNavWidget.js
@@ -0,0 +1,16 @@
+const SpotifyNavWidget = () => {
+	return (
+		<div className="flex flex-row items-center space-x-2 pr-2">
+			<div
+				style={{
+					background: `url('${'123'}') center / cover no-repeat`,
+					height: '30px',
+					aspectRatio: '1/1',
+				}}
+			/>
+			<span className="font-bold tracking-wide">{'123'}</span>
+		</div>
+	);
+};
+
+export default SpotifyNavWidget;
